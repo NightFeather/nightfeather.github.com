df2a3d0fe065edd1512e81f57778f2c30e899396
NightFeather <ss900328@gmail.com>
2014-05-01 02:34:29 +0800

    some tiny changes

--- pub_drafts.rb
+++ pub_drafts.rb
@@ -3,7 +3,7 @@
 Dir["_drafts/*"].each do |draft|
     newName = "_posts/" + File.mtime(draft).strftime("%Y-%m-%d-") + File.basename(draft)
     puts newName
-    File.open draft do |origin|
+    File.open draft do |origin| 
         File.open(newName,"w").write(origin.read)
     end
 end
